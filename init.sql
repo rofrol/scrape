32e6ff52f901e7d9394121deabb92b41643dfcaf
Roman Frołow <rofrol@gmail.com>
2016-03-08 04:06:42 +0100

    timestamp and populate.js

--- init.sql
+++ init.sql
@@ -3,5 +3,6 @@ create table stats (
   id serial primary key,
   hash varchar not null,
   seeds int not null,
-  peers int not null
+  peers int not null,
+  my_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 )
---
+++ populate.js
@@ -0,0 +1,41 @@
+require('./config.js');
+require('isomorphic-fetch');
+var pg = require('pg');
+var SQL = require('sql-template-strings')
+
+fetch('https://kat.cr/json.php?q=+category:tv&field=seeders&order=desc&page=1').then(response => response.json()).then(data => {
+
+  // console.log(data);
+
+  var client = new pg.Client();
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    queryCount = 0;
+    data.list.forEach(elem => {
+      console.log(elem);
+
+      var query = client.query(SQL`
+        INSERT
+        INTO    stats
+                (hash, seeds, peers)
+        VALUES  (${elem.hash}, ${elem.seeds}, ${elem.peers})
+      `);
+      queryCount++;
+      query.on('error', function(error) {
+        console.log(error);
+      });
+      query.on('end', function(result) {
+        queryCount--;
+        console.log("Query ended");
+        if (result) {
+           console.log("Added listitem no " + result.rows[0]);
+        }
+        if (queryCount === 0) {
+         client.end();
+        }
+      });
+    });
+  });
+});
